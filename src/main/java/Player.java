d5a0774f6a420923e4c878495db7531909a95b8b
Patrick Fürst <pfu@neongolden.net>
2013-04-19 17:06:38 +0200

    carposition added

--- src/main/java/Player.java
+++ src/main/java/Player.java
@@ -14,10 +14,14 @@ public class Player {
     
     
     private String name;
+    //define the position of the car
+    // zero if the car stands on the start
+    private int carPosition;
     
     
     public Player(String name){
         this.name = name;
+        this.carPosition = 0;
     }
     
     public String getName(){
@@ -27,6 +31,20 @@ public class Player {
     
     public void setName(String name){
         this.name = name;
+    }
+
+    /**
+     * @return the carPosition
+     */
+    public int getCarPosition() {
+        return carPosition;
+    }
+
+    /**
+     * @param carPosition the carPosition to set
+     */
+    public void setCarPosition(int carPosition) {
+        this.carPosition = carPosition;
     }
     
 }
