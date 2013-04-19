b9c05de64d73fb2ffae94ffd413f2396187cd381
Patrick Fürst <pfu@neongolden.net>
2013-04-19 16:57:07 +0200

    constructor added

--- src/main/java/Player.java
+++ src/main/java/Player.java
@@ -10,7 +10,15 @@
 public class Player {
     
     
+    
+    
+    
     private String name;
+    
+    
+    public Player(String name){
+        this.name = name;
+    }
     
     public String getName(){
     
