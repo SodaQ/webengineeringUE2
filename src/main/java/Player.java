86223df23b397f66a892fd92a55f939221c8a95d
Patrick Fürst <pfu@neongolden.net>
2013-04-19 16:05:54 +0200

    model class Player added

---
+++ src/main/java/Player.java
@@ -0,0 +1,24 @@
+/*
+ * This is our Model class Player.
+ *
+ */
+
+/**
+ *
+ * @author patrickfuerst
+ */
+public class Player {
+    
+    
+    private String name;
+    
+    public String getName(){
+    
+        return name;
+    }
+    
+    public void setName(String name){
+        this.name = name;
+    }
+    
+}
